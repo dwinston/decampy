1a4f6cb61fdfd40ca2c35a27fe7be3b8ec5618cd
Donny Winston <dwinston@alum.mit.edu>
2019-07-11 20:16:38 -0700

    Handle multiple choice

--- decampy/exercises.py
+++ decampy/exercises.py
@@ -1,10 +1,11 @@
 import logging
+import re
 from pathlib import Path
 from typing import Tuple, Dict
 
 from decampy.util import FileLines
 
-exercise_types = {"videoexercise": "slides"}
+exercise_types = {"videoexercise": "slides", "multiplechoiceexercise": "choice"}
 
 
 def get_slides(lines: FileLines) -> str:
@@ -46,6 +47,8 @@ def get_slides(lines: FileLines) -> str:
         elif within_slide and line.startswith("---"):
             content += f"---\n"
             within_slide = False
+        elif line.startswith("```{{"):  # unused metadata
+            content += "```\n"
         elif within_slide:
             content += f"{line}"
         i += 1
@@ -79,32 +82,33 @@ def get_exercise(
     chapter_path = basepath_out.joinpath("chapters", f"chapter{n_chapter}.md")
     processed = {chapter_path: ""}
     e_title, e_type = None, None
-    read_next_line_as_slide_hash = False
-    look_for_type = False
-    for i_line, line in enumerate(lines):
+    i = 0
+    while i < len(lines):
+        line = lines[i]
         if line.startswith("##"):
             e_title = " ".join(line.split()[1:]).strip()
         elif line.startswith("---"):
             processed[chapter_path] += f"</exercise>\n"
             break
         elif line.startswith("```yaml"):
-            look_for_type = True
-        elif look_for_type and line.startswith("```"):
-            look_for_type = False
-        elif look_for_type and line.startswith("type:"):
-            e_type_dc = line.split()[1].lower()
-            if e_type_dc in exercise_types:
-                e_type = exercise_types[e_type_dc]
-                processed[
-                    chapter_path
-                ] += f'<exercise id="{n}" title="{e_title}" type="{e_type}">\n'
-            else:
-                processed[chapter_path] += f'<exercise id="{n}" title="{e_title}">\n'
-            look_for_type = False
+            i += 1
+            while not lines[i].startswith("```"):
+                if lines[i].startswith("type:"):
+                    e_type_dc = lines[i].split()[1].lower()
+                    if e_type_dc in exercise_types:
+                        e_type = exercise_types[e_type_dc]
+                    if e_type == "slides":
+                        processed[
+                            chapter_path
+                        ] += f'<exercise id="{n}" title="{e_title}" type="{e_type}">\n'
+                    else:
+                        processed[
+                            chapter_path
+                        ] += f'<exercise id="{n}" title="{e_title}">\n'
+                i += 1
         elif e_type == "slides" and line.startswith("`@projector_key`"):
-            read_next_line_as_slide_hash = True
-        elif e_type == "slides" and read_next_line_as_slide_hash:
-            hash_ = line.strip()
+            i += 1
+            hash_ = lines[i].strip()
             slides_input_path = next(
                 p
                 for p in basepath_in.joinpath("slides").glob("*.md")
@@ -116,9 +120,50 @@ def get_exercise(
             slides_path = basepath_out.joinpath("slides", f"{slides_source}.md")
             processed[slides_path] = get_slides(slides_lines)
             processed[chapter_path] += f'<slides source="{slides_source}"></slides>\n'
-            read_next_line_as_slide_hash = False
+        elif e_type == "choice" and line.startswith("`@possible_answers"):
+            choices = []
+            i += 1
+            while lines[i].startswith("- "):
+                choices.append({"answer": re.match(r"- (.+)\n", lines[i]).group(1)})
+                i += 1
+            while not lines[i].startswith("`@sct"):
+                i += 1
+            i += 2
+            feedback = ""
+            while not lines[i].startswith("```"):
+                if not lines[i].startswith("Ex()"):
+                    feedback += lines[i]
+                else:
+                    correct_index = (
+                        int(
+                            re.search(
+                                r"has_chosen\(.*correct\s*=\s*(\d)", lines[i]
+                            ).group(1)
+                        )
+                        - 1
+                    )
+                    choices[correct_index]["correct"] = True
+                i += 1
+
+            d = {"msgs": []}
+            exec(feedback, d)
+            for j, msg in enumerate(d["msgs"]):
+                choices[j]["feedback"] = msg
+            processed[chapter_path] += "<choice>\n"
+            for choice in choices:
+                if "correct" in choice:
+                    processed[
+                        chapter_path
+                    ] += f'<opt text="{choice["answer"]}" correct="true">\n'
+                else:
+                    processed[chapter_path] += f'<opt text="{choice["answer"]}">\n'
+                processed[chapter_path] += f'{choice["feedback"]}\n</opt>\n'
+            processed[chapter_path] += "</choice>\n"
+        elif e_type == "choice":
+            processed[chapter_path] += line
         else:
             pass  # TODO: process non-slides exercise content
+        i += 1
 
     logging.debug(f"Done processing exerise {n} or chapter {n_chapter}")
-    return processed, lines[(i_line + 1) :]
+    return processed, lines[(i + 1) :]
